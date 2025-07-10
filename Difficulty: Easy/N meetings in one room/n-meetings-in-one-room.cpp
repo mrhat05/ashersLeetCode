2e397bb08d7c0905d9e31776417f089132aad170
Abhiram Sankarasetty <90583302+mrhat05@users.noreply.github.com>
2025-07-10 14:52:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxMeetings(vector<int>& start, vector<int>& end) {
+        int n = start.size();
+        vector<pair<int, int>> meetings;
+
+        for (int i = 0; i < n; i++) {
+            meetings.push_back({end[i], start[i]});
+        }
+
+        sort(meetings.begin(), meetings.end());
+
+        int count = 1;
+        int lastEnd = meetings[0].first;
+
+        for (int i = 1; i < n; i++) {
+            int currStart = meetings[i].second;
+            int currEnd = meetings[i].first;
+
+            if (currStart > lastEnd) {
+                count++;
+                lastEnd = currEnd;
+            }
+        }
+
+        return count;
+    }
+};
